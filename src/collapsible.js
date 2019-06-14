401110ad1150cd36aa4dab7a310a6deb3b2c2e28
bosk <dmitri@localhost>
2019-06-14 14:37:01 -0400

    update 'collapsible' to work with bootstrap 4 (backword compatible with bootstreap 3)

--- src/collapsible.js
+++ src/collapsible.js
@@ -1,4 +1,5 @@
 /**
+ * Collapsible constructor
  * Collapse navbar items dynamically, when there isn't enough space on the screen.
  *
  * @param {String} selector The selector for the items' container
@@ -15,7 +16,7 @@ function Collapsible(selector, threshold) {
 /**
  * Given the selector, query the items' container.
  * If it is unreachable, an exception is throwed.
- * 
+ *
  * @param {String} selector
  * @returns {*} The items' container element
  */
@@ -35,21 +36,19 @@ Collapsible.prototype.queryContainer = function (selector) {
  */
 Collapsible.prototype.getItemsDimensions = function () {
     var dimensions = [];
-
     for (var i = 0; i < this.items.length; i++) {
         var item  = this.items[i];
         var width = item.getBoundingClientRect().width;
 
         dimensions.push(width);
     }
-
     return dimensions;
 };
 
 /**
  * Calculate the available space based on the page width, and return
  * the amount of items we must collapse.
- * 
+ *
  * @returns {Number} Amount of items to collapse
  */
 Collapsible.prototype.getAmountOfItemsToCollapse = function () {
@@ -59,36 +58,37 @@ Collapsible.prototype.getAmountOfItemsToCollapse = function () {
     for (var i = 0; i < this.items.length; i++) {
         var itemWidth = this.itemsDimensions[i];
         renderable.width += itemWidth;
-
         if ((pageWidth - renderable.width) < this.threshold)
             break;
-
         renderable.amount++;
     }
-
     return this.items.length - renderable.amount;
 };
 
 Collapsible.prototype.collapse = function () {
     var menu = this.container.querySelector('.collapsible-menu');
     var dropdown = menu.querySelector('.collapsible-dropdown');
-    var dropdownItems = dropdown.querySelectorAll('li');
+    var dropdownItems = dropdown.querySelectorAll('li .nav-item');
     var amountOfItemsToCollapse = this.getAmountOfItemsToCollapse();
+    //console.log(amountOfItemsToCollapse + " items to collapse");
 
-    // Default case on large screens and when there is enough space
-    menu.classList.remove('hide');
-    
+    /**
+     * Default case on large screens and when there is enough space
+     * hide dropdown menu when there is nothing to collapse
+     * (bootstrap 3: 'hidden', bootstrap 4: 'd-none')
+     */
+    menu.classList.remove('hidden', 'd-none');
     if (amountOfItemsToCollapse === 0) {
-        menu.classList.add('hide');
+        menu.classList.add('hidden', 'd-none');
     }
 
     // Clear the state for each item: both in the original list and in the collapsed one
     for (var i = 0; i < this.items.length; i++) {
-    var item = this.items[i];
+        var item = this.items[i];
         var clone = dropdownItems[i];
 
-        item.classList.remove('hide');
-        clone.classList.add('hide');
+        item.classList.remove('hidden', 'd-none');
+        clone.classList.add('hidden', 'd-none');
     }
 
     for (var i = 0; i < amountOfItemsToCollapse; i++) {
@@ -96,9 +96,10 @@ Collapsible.prototype.collapse = function () {
         var item = this.items[index];
         var clone = dropdownItems[index];
 
-        item.classList.add('hide');
-        clone.classList.remove('hide');
+        item.classList.add('hidden', 'd-none');
+        clone.classList.remove('hidden', 'd-none');
     }
+
 };
 
 /**
@@ -107,38 +108,40 @@ Collapsible.prototype.collapse = function () {
  * You should customize this method based on your needs.
  */
 Collapsible.prototype.inject = function () {
-    var menu = document.createElement('li');
-    menu.classList.add('collapsible-menu');
-
-    var dropdownList = document.createElement('ul');
-    dropdownList.classList.add('dropdown-menu', 'collapsible-dropdown-list');
-
-    var icon = document.createElement('span');
-    icon.classList.add('fas', 'fa-bars');
-
-    var dropdown = document.createElement('div');
-    dropdown.classList.add('dropdown', 'collapsible-dropdown');
-    
+    // toggler button
     var button = document.createElement('button');
-    button.classList.add('btn', 'dropdown-toggle', 'collapsible-toggle');
-
+    button.classList.add('btn', 'btn-outline-secondary', 'dropdown-toggle', 'collapsible-toggle');
+    button.setAttribute('data-toggle', 'dropdown')
+    /*
     // Attach event to toggle dropdown menu, without using default Bootstrap jQuery-based script
     button.addEventListener('click', function (ev) {
-        ev.preventDefault();
-        dropdown.classList.toggle('open');
-    });
+    ev.preventDefault();
+    //dropdown.classList.toggle('open'); // bootstrap 3
+    //menu.classList.toggle('show'); // bootstrap 4
+    });*/
+
+    // .dropdown-menu
+    var dropdownList = document.createElement('ul');
+    dropdownList.classList.add('collapsible-dropdown-list', 'list-unstyled');
+    //addLink(dropdownList, "MORE");
+
+    // create collapsible menu container: .dropdown
+    var dropdown = document.createElement('div');
+    dropdown.classList.add('dropdown-menu', 'collapsible-dropdown');
+
+    var menu = document.createElement('li');
+    menu.classList.add('nav-item', 'dropdown', 'collapsible-menu');
 
     // Copy items from the original navbar
     for (var i = 0; i < this.items.length; i++) {
         var item = this.items[i];
         var clone = item.cloneNode(true);
 
-        clone.classList.add('hide');
+        clone.classList.add('hidden', 'd-none');
         dropdownList.append(clone);
     }
 
-    button.append(icon);
-    dropdown.append(button);
+    menu.append(button);
     dropdown.append(dropdownList);
 
     menu.append(dropdown);
@@ -146,12 +149,30 @@ Collapsible.prototype.inject = function () {
 };
 
 /**
+ *  @param {HTMLUListElement} dump list into console log
+*/
+function dumpList(lst) {
+    let children = lst.children;
+    for(var i = 0; i < lst.length; i++) {
+        console.log(lst[i].innerHTML);
+    }
+}
+
+function addLink(lst, text) {
+    var a = document.createElement('a');
+    var linkText = document.createTextNode(text);
+    a.appendChild(linkText);
+    a.title = text;
+    lst.append(a);
+}
+
+/**
  * Initialize the resize sensor and assign it the right callback,
- * containing the code to collapse exceeding items when necessary.
+ * containing the code to collapse exceeding items when necessary
  */
 Collapsible.prototype.render = function () {
     this.inject();
     this.collapse();
-
-    new ResizeSensor(this.container.parentElement, this.collapse.bind(this));
+    new ResizeSensor(this.container.parentElement,
+                     this.collapse.bind(this));
 };
